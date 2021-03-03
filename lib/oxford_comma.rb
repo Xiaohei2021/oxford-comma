b0fc62f0810688b617a090350b08f90b3fbb0006
Leon <hualiang.tian@gmail.com>
2021-03-03 16:19:32 -0500

    commit tree fcf1f0072960782a1dd46200e71dcbaf435417d5

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,37 @@
+require 'pry'
+# def oxford_comma(array)
+
+#     if array.length == 2 
+#         array.join(" and ")
+
+#     elsif array.length >= 3
+        
+#         [array[0..-2].join(", "), array.last].join(", and ")
+    
+#     else array.join(",")
+#     end
+
+# end 
+
 def oxford_comma(array)
+    
+     case array.count
+        when 0 
+            "" 
+    binding.pry
+        when 1
+            array.join()
+        when 2
+            array.join(" and ")
+        when 3
+            
+            [array[0..1].join(", "), array.last] .join(", and ") 
+            #binding.pry
+        else 
+            [array[0...-1].join(", "), array.last] .join(", and ") 
+            #binding.pry
+
+    end
 
 end
+   
